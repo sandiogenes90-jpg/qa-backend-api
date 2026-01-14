188cb2496853a5dfaa8a4b0f051dd09f7d2dfb69
Diogenes <josed@Jos.localdomain>
2026-01-14 15:05:23 -0300

    refactor: add BaseApiTest and clean test structure

---
+++ src/test/kotlin/api/BaseApiTest.kt
@@ -0,0 +1,16 @@
+package api
+
+import io.restassured.RestAssured
+import org.junit.jupiter.api.BeforeAll
+
+abstract class BaseApiTest {
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            RestAssured.baseURI = "http://localhost"
+            RestAssured.port = 8080
+        }
+    }
+}
